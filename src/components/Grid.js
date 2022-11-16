116bdb575bb25ec3357e35d54ac9de07ec00bd21
eurorondon03 <eurorondon03@gmail.com>
2022-11-16 00:04:01 -0400

    cambiando cuadricula de productos en shopsections, remplazado a GRID, adelante un poco mas

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,24 +1,39 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Navigate } from "react-router";
+import { Link } from "react-router-dom";
 
 const Grid = () => {
   const productList = useSelector((state) => state.productList);
   const { products } = productList;
-  console.log(products);
+
+  const navigatehandle = (id) => {
+    <Navigate to={`/products/${id}`} />;
+    console.log(id);
+  };
+
   return (
     <>
-      <div className="">
+      <div className="shoptext">
         <div className="grid my-5">
           {products.map((product) => (
-            <div
-              className="cardpen"
-              style={{
-                backgroundImage:
-                  "url(https://images1.vinted.net/t/02_0054a_2M5XXSEdM7i4rJwfNjKYDY6b/f800/1666960795.jpeg?s=c69fb78425c7c2bba4ca40af123e20bfaf13976d)",
-              }}
-            >
-              <div className="card__details">Jobs To Be Done Workseet</div>
-            </div>
+            <>
+              <div
+                className="cardpen"
+                onClick={() => {
+                  navigatehandle(product._id);
+                }}
+                style={{
+                  backgroundImage: ` url(${product.image[0]})`,
+                }}
+              >
+                <Link to={`/products/${product._id}`}>
+                  <div className="card__details d-flex justify-content-between align-items-end ">
+                    {product.name} <h5>${product.price}</h5>
+                  </div>
+                </Link>
+              </div>
+            </>
           ))}
         </div>
       </div>
@@ -27,3 +42,29 @@ const Grid = () => {
 };
 
 export default Grid;
+
+// {
+//   products.map((product) => (
+//     <div className="shop col-6 col-xl-4 col-lg-6" key={product._id}>
+//       <div className="border-product">
+//         <Link to={`/products/${product._id}`}>
+//           <div className="shopBack">
+//             <img src={product.image[0]} alt={product.name} />
+//           </div>
+//         </Link>
+
+//         <div className="shoptext">
+//           <p>
+//             <Link to={`/products/${product._id}`}>{product.name}</Link>
+//           </p>
+
+//           <Rating
+//             value={product.rating}
+//             text={`${product.numReviews} reviews`}
+//           />
+//           <h3>${product.price}</h3>
+//         </div>
+//       </div>
+//     </div>
+//   ));
+// }
